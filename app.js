e50005bdd89074621a66b3667735576fbf9ffbcc
core.pager=cat <shinaayomibamgboye@gmail.com>
2023-03-29 17:19:57 +0100

    scroll added

--- app.js
+++ app.js
@@ -2,3 +2,10 @@ let stars = document.getElementById("stars")
 let moon = document.getElementById("moon")
 let mountain_back = document.getElementById("mountain_back")
 let mountain_front = document.getElementById("mountain_front")
+let btn = document.getElementsByClassName("btn")
+let text = document.getElementById("text")
+
+window.addEventListener('scroll',function(){
+    let value = window.scrollY;
+    
+})
