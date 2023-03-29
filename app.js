3037c760a52e33bf598042ece8df222f31063957
core.pager=cat <shinaayomibamgboye@gmail.com>
2023-03-29 17:07:56 +0100

    javascript

---
+++ app.js
@@ -0,0 +1,4 @@
+let stars = document.getElementById("stars")
+let moon = document.getElementById("moon")
+let mountain_back = document.getElementById("mountain_back")
+let mountain_front = document.getElementById("mountain_front")
