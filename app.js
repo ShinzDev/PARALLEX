2be4a0dc31a978247739d03d31be28d5707b5c8b
core.pager=cat <shinaayomibamgboye@gmail.com>
2023-03-31 10:40:23 +0100

    done

--- app.js
+++ app.js
@@ -7,5 +7,13 @@ let text = document.getElementById("text")
 
 window.addEventListener('scroll',function(){
     let value = window.scrollY;
-    
+    stars.style.left = value * 0.25+'px';  
+    moon.style.top = value * 1.05+'px'; 
+    mountain_back.style.top = value * 0.5+'px'; 
+    mountain_front.style.top = value * 0+'px'; 
+    text.style.marginTop = value *1.5 +'px';
+    text.style.marginRight = value *4+'px';
+    btn.style.marginTop = value * +'px';
+    // moon.style.down = value +'px';
+
 })
